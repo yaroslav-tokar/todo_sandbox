e1f32a0a9b64cd8ad8dbd78110f4a0971c93fb3e
Yaroslav Tokar <yaroslav.tokar@faifly.com>
2021-08-17 15:15:08 +0300

    fix issue with di

---
+++ lib/core/constants/common.dart
@@ -0,0 +1,1 @@
+const String applicationDatabaseName = 'app_db';
--- lib/presentation/di/di.dart
+++ lib/presentation/di/di.dart
@@ -1,7 +1,9 @@
 import 'package:get_it/get_it.dart';
+import 'package:todo_sandbox/core/constants/common.dart';
 import 'package:todo_sandbox/data/datasource/local/db/app_db.dart';
 import 'package:todo_sandbox/data/repositories/note_repository_impl.dart';
 import 'package:todo_sandbox/domain/provider/note_local_data_provider.dart';
+import 'package:todo_sandbox/domain/repositories/note_repository.dart';
 import 'package:todo_sandbox/domain/use_case/note_use_case/create_note_use_case.dart';
 import 'package:todo_sandbox/domain/use_case/note_use_case/delete_note_use_case.dart';
 import 'package:todo_sandbox/domain/use_case/note_use_case/get_all_notes_use_case.dart';
@@ -13,35 +15,28 @@ final GetIt locator = GetIt.instance;
 //TODO fix di issue: currently ioc container does not create dependencies for
 // repositories
 Future<void> setupDi() async {
-  await _initDb();
-  await _initDataProviders();
-  await _initRepositories();
-  await _initBlocks();
-  await _initUseCases();
-}
+  //Register db instance
+  final AppDatabase database = await $FloorAppDatabase.databaseBuilder(applicationDatabaseName).build();
+  locator.registerSingleton<AppDatabase>(database);
 
-Future<void> _initBlocks() async {
-  locator.registerFactory<HomeBlock>(() => HomeBlock(locator()));
-}
+  final NoteLocalDataProvider localDataProvider = NoteLocalDataProvider(locator());
+  locator.registerSingleton<NoteLocalDataProvider>(localDataProvider);
 
-Future<void> _initUseCases() async {
+  //Register remote api service
+  final NoteRemoteDataProvider remoteDataProvider = NoteRemoteDataProvider();
+  locator.registerSingleton<NoteRemoteDataProvider>(remoteDataProvider);
+
+  //Register repository
+  locator.registerSingleton<NoteRepository>(
+    NoteRepositoryImpl(locator(), locator()),
+  );
+
+  //Register usecases
   locator.registerSingleton<GetAllNotesUseCase>(GetAllNotesUseCase(locator()));
   locator.registerSingleton<UpdateNoteUseCase>(UpdateNoteUseCase(locator()));
   locator.registerSingleton<DeleteNoteUseCase>(DeleteNoteUseCase(locator()));
   locator.registerSingleton<CreateNoteUseCase>(CreateNoteUseCase(locator()));
-}
 
-Future<void> _initDataProviders() async {
-  locator.registerSingleton<NoteLocalDataProvider>(
-      NoteLocalDataProvider(locator()));
-}
-
-Future<void> _initRepositories() async {
-  locator.registerSingleton<NoteRepositoryImpl>(
-      NoteRepositoryImpl(locator(), locator()));
-}
-
-Future<void> _initDb() async {
-  final appDb = await $FloorAppDatabase.databaseBuilder('app_db').build();
-  locator.registerSingleton<AppDatabase>(appDb);
+  //Register BloCs
+  locator.registerFactory<HomeBlock>(() => HomeBlock(locator()));
 }
